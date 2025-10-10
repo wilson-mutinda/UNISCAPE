3c49297dd9a7937886413885d4f26d8e39d06dcf
Wilson Kilonzo <mutidawilz@gmail.com>
2025-10-10 09:40:46 +0300

    mobile menu render from left

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -9,18 +9,18 @@
                      <div class="flex items-center gap-2">
                         <img src="/library.png" alt="library" width="50">
                         <div class="">
-                            <h1 class="text-3xl text-[#1B325E]">UNISCAPE</h1>
+                            <h1 class="text-3xl text-uniscape-blue">UNISCAPE</h1>
                             <p>-- CONSULTING --</p>
                         </div>
                      </div>
                      <!-- menu -->
                       <div class="hidden md:flex items-center gap-4">
-                        <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#">Home</a>
-                        <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#about">About Us</a>
-                        <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#programs">Programs</a>
-                        <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#community">Community</a>
-                        <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#contacts">Contact Us</a>
-                        <a class="text-lg text-[#E5A233] rounded-md bg-[#1B325E] px-4 py-2" href="#">Get Started</a>
+                        <a class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2" href="#">Home</a>
+                        <a class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2" href="#about">About Us</a>
+                        <a class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2" href="#programs">Programs</a>
+                        <a class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2" href="#community">Community</a>
+                        <a class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2" href="#contacts">Contact Us</a>
+                        <a class="text-lg text-uniscape-yellow rounded-md bg-uniscape-blue px-4 py-2" href="#">Get Started</a>
                       </div>
 
                       <!-- mobile menu -->
@@ -32,27 +32,33 @@
                  </div>
 
                  <!-- mobile menu component -->
-                  <div v-if="showMenu" class="flex flex-col items-center gap-1 md:hidden bg-gray-50">
-                    <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#">Home</a>
-                    <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#about">About Us</a>
-                    <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#programs">Programs</a>
-                    <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#community">community</a>
-                    <a class="text-[#1B325E] text-lg hover:text-[#E5A233] rounded-md hover:bg-[#1B325E] px-4 py-2" href="#contacts">Contact Us</a>
-                    <a class="text-lg text-[#E5A233] rounded-md bg-[#1B325E] px-4 py-2" href="#">Get Started</a>
-                   </div>
+                  <transition name="slide">
+                    <div v-if="showMenu" class="fixed inset-0 left-0 w-64 bg-uniscape-yellow shadow-lg z-40 flex flex-col items-start p-6 space-y-3 md:hidden">
+                        <button @click="toggleMenu" type="button" class="self-end mb-4 text-uniscape-blue font-bold">
+                            <img src="/close.png" alt="close" width="30">
+                        </button>
+
+                        <a @click="toggleMenu" class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2 w-full text-left" href="#">Home</a>
+                        <a @click="toggleMenu" class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2 w-full text-left" href="#about">About Us</a>
+                        <a @click="toggleMenu" class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2 w-full text-left" href="#programs">Programs</a>
+                        <a @click="toggleMenu" class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2 w-full text-left" href="#community">Community</a>
+                        <a @click="toggleMenu" class="text-uniscape-blue text-lg hover:text-uniscape-yellow rounded-md hover:bg-uniscape-blue px-4 py-2 w-full text-left" href="#contacts">Contact Us</a>
+                        <a @click="toggleMenu" class="text-lg text-uniscape-yellow rounded-md bg-uniscape-blue px-4 py-2 w-full text-center" href="#">Get Started</a>
+                    </div>
+                  </transition>
             </div>
         </nav>
 
         <!-- hero section -->
-        <section id="#" class="py-16 bg-gradient-to-r from-[#1B325E] to-blue-900 pt-24">
+        <section id="#" class="py-16 bg-gradient-to-r from-uniscape-blue to-blue-900 pt-24">
             <div class="container mx-auto px-4">
-                <h1 class="text-[#E5A233] text-center text-5xl font-bold mb-4 mt-4">Empowering Next Tech Talent</h1>
+                <h1 class="text-uniscape-yellow text-center text-5xl font-bold mb-4 mt-4">Empowering Next Tech Talent</h1>
                 <p class="text-white text-center mx-auto max-w-2xl text-xl mb-4">Data-driven insights, digital transformation, hands-on training programs designed for real-world success</p>
 
                 <!-- buttons -->
                  <div class="flex items-center justify-center gap-4">
-                    <button type="button" class="bg-[#E5A233] text-blue-900 px-5 py-3 rounded-md font-semibold shadow-lg hover:bg-yellow-300 transition">Get Started</button>
-                    <button type="button" class="border border-[#E5A233] text-white px-5 py-3 rounded-md font-semibold hover:bg-[#E5A233] hover:text-blue-900 transition">Learn More</button>
+                    <button type="button" class="bg-uniscape-yellow text-blue-900 px-5 py-3 rounded-md font-semibold shadow-lg hover:bg-yellow-300 transition">Get Started</button>
+                    <button type="button" class="border border-uniscape-yellow text-white px-5 py-3 rounded-md font-semibold hover:bg-uniscape-yellow hover:text-blue-900 transition">Learn More</button>
                  </div>
             </div>
         </section>
@@ -60,17 +66,17 @@
         <!-- about section -->
         <section id="about" class="bg-gray-50 py-16">
             <div class="container mx-auto px-4">
-                <h3 class="text-3xl text-center font-semibold text-[#1B325E] mb-12">About Us</h3>
+                <h3 class="text-3xl text-center font-semibold text-uniscape-blue mb-12">About Us</h3>
 
                 <!-- about info -->
                  <div class="flex flex-col items-center justify-between gap-8 md:flex-row">
                     <!-- logo -->
-                     <div class="bg-gradient-to-br from-[#1B325E] to-blue-900 rounded-xl w-full md:w-1/2 flex justify-center items-center p-6 shadow-lg">
+                     <div class="bg-gradient-to-br from-uniscape-blue to-blue-900 rounded-xl w-full md:w-1/2 flex justify-center items-center p-6 shadow-lg">
                         <img src="/library.png" alt="library" width="300">
                      </div>
                      <!-- info -->
                       <div class="w-full md:w-1/2">
-                        <h3 class="font-bold text-2xl text-center mb-4 text-[#1B325E]">Why Choose NETSCAPE?</h3>
+                        <h3 class="font-bold text-2xl text-center mb-4 text-uniscape-blue">Why Choose NETSCAPE?</h3>
                         <p class="text-lg text-gray-600 mb-4">Uniscape Consulting is a professional training and technology 
                             solutions company dedicated to bridging Africa&apos;s digital skill gap. Through 
                             hands-on bootcamps, mentorship, and project-based learning, we empower learners 
@@ -112,25 +118,25 @@
         <!-- programs section -->
         <section id="programs" class="py-16 bg-white">
             <div class="container mx-auto px-4">
-                <h3 class="text-3xl text-center font-semibold text-[#1B325E] mb-12">Our Programmes</h3>
+                <h3 class="text-3xl text-center font-semibold text-uniscape-blue mb-12">Our Programmes</h3>
 
                 <!-- featured courses -->
                  <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                    <div v-for="(course, index) in featured" :key="course.id" class="bg-gradient-to-b from-[#E5A233] to-yellow-50 rounded-2xl shadow-lg p-6 hover:-translate-y-2 transition-transform duration-300">
-                        <h4 class="text-2xl font-bold text-[#1B325E] mb-3">{{ course.name }}</h4>
+                    <div v-for="(course, index) in featured" :key="course.id" class="bg-gradient-to-b from-uniscape-yellow to-yellow-50 rounded-2xl shadow-lg p-6 hover:-translate-y-2 transition-transform duration-300">
+                        <h4 class="text-2xl font-bold text-uniscape-blue mb-3">{{ course.name }}</h4>
                         <p class="text-gray-700 mb-2">
-                            <span class="font-semibold text-[#1B325E]">Duration:</span> {{ course.duration }} months
+                            <span class="font-semibold text-uniscape-blue">Duration:</span> {{ course.duration }} months
                         </p>
 
                         <p class="text-gray-700 mb-4">
-                            <span class="font-semibold text-[#1B325E]">Fees:</span> KES {{ course.fees }}
+                            <span class="font-semibold text-uniscape-blue">Fees:</span> KES {{ course.fees }}
                         </p>
 
                         <p class="text-gray-600 mb-6">
                             Learn the latest tools and skills in {{ course.name }} with hands on projects, expert mentors, and a global-ready curriculum.
                         </p>
 
-                        <button type="button" class="bg-[#1B325E] text-yellow-300 px-5 py-2 rounded-md font-semibold hover:bg-blue-900 transition">
+                        <button type="button" class="bg-uniscape-blue text-yellow-300 px-5 py-2 rounded-md font-semibold hover:bg-blue-900 transition">
                             Explore Course
                         </button>
                     </div>
@@ -141,7 +147,7 @@
         <!-- our partners -->
          <section class="bg-gray-50 py-16">
             <div class="container mx-auto px-4">
-                <h3 class="text-3xl text-center font-semibold text-[#1B325E] mb-4">Our Partners</h3>
+                <h3 class="text-3xl text-center font-semibold text-uniscape-blue mb-4">Our Partners</h3>
 
                 <!-- partners -->
                  <div class="flex items-center space-x-10 animate-marquee">
@@ -158,14 +164,14 @@
         <!-- contact section -->
         <section id="contacts" class="bg-gray-50 py-20">
         <div class="container mx-auto px-4">
-            <h3 class="text-3xl text-center font-semibold text-[#1B325E] mb-12">
+            <h3 class="text-3xl text-center font-semibold text-uniscape-blue mb-12">
             Contact Us
             </h3>
 
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
             <!-- contact form -->
             <div class="bg-white shadow-lg rounded-lg p-6">
-                <h3 class="font-bold text-2xl mb-4 text-[#1B325E]">
+                <h3 class="font-bold text-2xl mb-4 text-uniscape-blue">
                 Send us a message
                 </h3>
 
@@ -174,7 +180,7 @@
                 <div>
                     <label class="block text-lg mb-1 font-medium" for="name">Name</label>
                     <input
-                    class="w-full p-3 rounded-md ring-1 outline-none ring-gray-300 hover:ring-[#1B325E] focus:ring-[#1B325E]"
+                    class="w-full p-3 rounded-md ring-1 outline-none ring-gray-300 hover:ring-uniscape-blue focus:ring-uniscape-blue"
                     type="text"
                     name="name"
                     id="name"
@@ -186,7 +192,7 @@
                 <div>
                     <label class="block text-lg mb-1 font-medium" for="email">Email</label>
                     <input
-                    class="w-full p-3 rounded-md ring-1 outline-none ring-gray-300 hover:ring-[#1B325E] focus:ring-[#1B325E]"
+                    class="w-full p-3 rounded-md ring-1 outline-none ring-gray-300 hover:ring-uniscape-blue focus:ring-uniscape-blue"
                     type="email"
                     name="email"
                     id="email"
@@ -198,7 +204,7 @@
                 <div>
                     <label class="block text-lg mb-1 font-medium" for="message">Message</label>
                     <textarea
-                    class="w-full p-3 rounded-md ring-1 outline-none ring-gray-300 hover:ring-[#1B325E] focus:ring-[#1B325E]"
+                    class="w-full p-3 rounded-md ring-1 outline-none ring-gray-300 hover:ring-uniscape-blue focus:ring-uniscape-blue"
                     name="message"
                     id="message"
                     rows="4"
@@ -208,7 +214,7 @@
 
                 <!-- submit button -->
                 <button
-                    class="w-full rounded-md bg-[#1B325E] hover:bg-[#27457D] p-3 text-white font-semibold transition duration-300"
+                    class="w-full rounded-md bg-uniscape-blue hover:bg-[#27457D] p-3 text-white font-semibold transition duration-300"
                 >
                     Send Message
                 </button>
@@ -217,14 +223,14 @@
 
             <!-- contact info -->
             <div class="bg-white shadow-lg rounded-lg p-6">
-                <h3 class="font-bold text-2xl mb-6 text-[#1B325E]">Get in Touch</h3>
+                <h3 class="font-bold text-2xl mb-6 text-uniscape-blue">Get in Touch</h3>
 
                 <div class="space-y-6">
                 <!-- Address -->
                 <div class="flex gap-4 items-start">
                     <img src="/map-pin-house.png" alt="address" width="30" class="mt-1" />
                     <div>
-                    <h4 class="font-semibold text-lg text-[#1B325E] mb-1">Address</h4>
+                    <h4 class="font-semibold text-lg text-uniscape-blue mb-1">Address</h4>
                     <p class="text-gray-700 leading-relaxed">
                         Blue Violets Plaza, Ground Floor, Kindaruma Road (off Ngong Road)<br />
                         P.O. Box 56936 - 00200, Nairobi, Kenya
@@ -236,7 +242,7 @@
                 <div class="flex gap-4 items-start">
                     <img src="/phone.png" alt="phone" width="30" class="mt-1" />
                     <div>
-                    <h4 class="font-semibold text-lg text-[#1B325E] mb-1">Phone</h4>
+                    <h4 class="font-semibold text-lg text-uniscape-blue mb-1">Phone</h4>
                     <p class="text-gray-700">+254 700 223 222</p>
                     <p class="text-gray-700">+254 729 146 152</p>
                     </div>
@@ -246,10 +252,10 @@
                 <div class="flex gap-4 items-start">
                     <img src="/mail.png" alt="email" width="30" class="mt-1" />
                     <div>
-                    <h4 class="font-semibold text-lg text-[#1B325E] mb-1">Email</h4>
+                    <h4 class="font-semibold text-lg text-uniscape-blue mb-1">Email</h4>
                     <a
                         href="mailto:info@uniscape.co.ke"
-                        class="text-gray-700 hover:text-[#1B325E] underline"
+                        class="text-gray-700 hover:text-uniscape-blue underline"
                     >
                         info@uniscape.co.ke
                     </a>
@@ -273,10 +279,10 @@
         </section>
 
         <!-- footer -->
-        <footer class="py-14 bg-[#1B325E]">
+        <footer class="py-14 bg-uniscape-blue">
             <div class="container px-4 mx-auto">
                 <!-- copyright -->
-                 <p class="text-lg text-[#E5A233]">© {{ new Date().getFullYear()}} Uniscape Consulting. All rights reserved.</p>
+                 <p class="text-lg text-uniscape-yellow">© {{ new Date().getFullYear()}} Uniscape Consulting. All rights reserved.</p>
             </div>
         </footer>
 
@@ -324,7 +330,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     @keyframes marquee {
     0% {
         transform: translateX(0%);
@@ -342,6 +348,31 @@ export default {
 
     .animate-marquee:hover {
     animation-play-state: paused;
+    }
+
+    .slide-enter-active,
+    .slide-leave-active {
+        transition: transform 0.4s ease, opacity 0.3s ease;
+    }
+
+    .slide-enter-from {
+        transform: translateX(-100%);
+        opacity: 0;
+    }
+
+    .slide-enter-to {
+        transform: translateX(0);
+        opacity: 1;
+    }
+
+    .slide-leave-from {
+        transform: translateX(0);
+        opacity: 1;
+    }
+
+    .slide-leave-to {
+        transform: translateX(-100%);
+        opacity: 0;
     }
 
 </style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'uniscape-blue': '#1B325E',
+        'uniscape-yellow': '#E5A233'
+      }
+    },
   },
   plugins: [],
 }
